fc7ae385eb344a4fcdca1e9bf04f8e7f7ef8b55f
zzj <WWW1121c@126.com>
2023-11-09 17:59:00 +0800

    new:icon

--- src/components/map/leafletMap.vue
+++ src/components/map/leafletMap.vue
@@ -6,14 +6,15 @@ import * as L from 'leaflet';
 import 'leaflet.chinatmsproviders';
 import 'leaflet.pm'
 import 'leaflet.pm/dist/leaflet.pm.css'
+import icon_green from '@/assets/img/icon_ld.png' 
 
 let key: any = '2973b4ae6fc25a6f326754a6ffc6eccc'
 
 const initMap = () => {
-    
+
     //卫星地图
     const leaflet = L.tileLayer(`http://t0.tianditu.gov.cn/cia_w/wmts?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=cia&STYLE=default&TILEMATRIXSET=w&FORMAT=tiles&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}&tk=${key}`)
-    
+
     //标注
     const leafletText = L.tileLayer(`http://t0.tianditu.gov.cn/cia_w/wmts?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=cia&STYLE=default&TILEMATRIXSET=w&FORMAT=tiles&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}&tk=${key}`)
 
@@ -27,36 +28,37 @@ const initMap = () => {
         })
 
     ]);
-    //标点
-    var marker = L.marker([	24.6438, 110.6467])
-    var greenIcon = L.icon({
-    iconUrl: '@/assets/img/icon_ld.png',
-    //shadowUrl: 'leaf-shadow.png',
-
-    iconSize:     [38, 95], // size of the icon
-    shadowSize:   [50, 64], // size of the shadow
-    iconAnchor:   [24.6438, 110.6467], // point of the icon which will correspond to marker's location
-    shadowAnchor: [4, 62],  // the same for the shadow
-    popupAnchor:  [24.6438, 110.6467] // point from which the popup should open relative to the iconAnchor
-    });
-
-
-    const layers = L.layerGroup([leaflet, leafletText])
     
+        
+    var greenIcon = L.icon({
+        iconUrl: icon_green,
+        //shadowUrl: 'leaf-shadow.png',
 
+        iconSize: [63, 74], // size of the icon
+        iconAnchor: [31,74], // point of the icon which will correspond to marker's location
+        //shadowSize: [50, 64], // size of the shadow
+        //shadowAnchor: [4, 62],  // the same for the shadow
+        popupAnchor: [0,0] // point from which the popup should open relative to the iconAnchor
+    });
+    //satelliteTileLayer.addTo(map);
+
+    //标点
+    var marker  = L.marker([24.6438, 110.6467])
+    var marker1 = L.marker([24.6438, 110.6467],{icon: greenIcon})
+
+    const layers = L.layerGroup([leaflet, leafletText,satelliteTileLayer])
+    // https://leafletjs.cn/reference.html#map-option
     let map = L.map('myMap', {//绑定地图容器的id
-        center: [24.8, 110.5], //缩放等级
-        zoom: 8,
+        center: [24.8, 110.5], //地图初始化时的中心点位置
+        zoom: 8,//地图初始化时的缩放等级
         maxZoom: 18,
         minZoom: 3,
         zoomControl: false,
-        layers: [layers,marker],
+        layers: [layers,marker,marker1 ],//默认添加到地图上的图层组
         attributionControl: false,  // 移除右下角leaflet标识
     })
 
-    satelliteTileLayer.addTo(map);
-
-
+    
     // 设置绘制后的线条颜色等
     map.pm.setPathOptions({
         color: "orange",
@@ -90,7 +92,6 @@ onMounted(() => {
 <template>
     <!-- 地图容器 -->
     <div id="myMap" style=" width:1920px ; height:1080px ;z-index: 2"></div>
-    
 </template>
 
 <style scoped></style>
