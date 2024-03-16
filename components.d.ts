e083f26e1d8b2c0841c896d531bacb0e4e1122c9
Scccccccc <WWW1121c@126.com>
2024-03-16 19:12:35 +0800

    new: beta滑块

--- components.d.ts
+++ components.d.ts
@@ -9,20 +9,7 @@ declare module 'vue' {
   export interface GlobalComponents {
     BaseMap: typeof import('./src/components/base-map/base-map.vue')['default']
     Components: typeof import('./src/components/map2/components/index.vue')['default']
-    ElButton: typeof import('element-plus/es')['ElButton']
-    ElCol: typeof import('element-plus/es')['ElCol']
-    ElDescriptions: typeof import('element-plus/es')['ElDescriptions']
-    ElDescriptionsItem: typeof import('element-plus/es')['ElDescriptionsItem']
-    ElDivider: typeof import('element-plus/es')['ElDivider']
-    ElFormItem: typeof import('element-plus/es')['ElFormItem']
     ElIcon: typeof import('element-plus/es')['ElIcon']
-    ElImage: typeof import('element-plus/es')['ElImage']
-    ElOption: typeof import('element-plus/es')['ElOption']
-    ElRow: typeof import('element-plus/es')['ElRow']
-    ElScrollbar: typeof import('element-plus/es')['ElScrollbar']
-    ElTable: typeof import('element-plus/es')['ElTable']
-    ElTableColumn: typeof import('element-plus/es')['ElTableColumn']
-    ElTree: typeof import('element-plus/es')['ElTree']
     ElTreeSelect: typeof import('element-plus/es')['ElTreeSelect']
     LeafletMap: typeof import('./src/components/map/leafletMap.vue')['default']
     LeafletMap1: typeof import('./src/components/map2/components/leafletMap1.vue')['default']
@@ -31,8 +18,5 @@ declare module 'vue' {
     MapBox: typeof import('./src/components/map-box/map-box.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
-  }
-  export interface ComponentCustomProperties {
-    vLoading: typeof import('element-plus/es')['ElLoadingDirective']
   }
 }
