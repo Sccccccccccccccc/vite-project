871e5275fff37ba5f1c8c6f73aac99ff39e3429d
zzj <www1121c@126.com>
2024-12-12 16:07:49 +0800

    feat: easyplayer播放器

--- src/views/blankPage/blankPage.vue
+++ src/views/blankPage/blankPage.vue
@@ -1,7 +1,7 @@
 <template>
 
-  <div style="background-color: aliceblue; width: 50%; position: relative; left: 50%; transform: translateX(-50%); top: 34%;">
-    <div class="dataList-group-box">
+  <div style="display: flex; background-color: aliceblue; width: 70%; position: relative; left: 50%; transform: translateX(-50%); top: 34%;">
+    <!-- <div class="dataList-group-box">
       <el-row :gutter="20" class="dataList-group">
         <el-col :lg="12" :md="24" class="dataList">
           <scrollTable :tableDataList="inboundDataList" :tableScrollSpeed="1" tableTitle="⬆️近20条入库记录" :itemNumber="10">
@@ -9,14 +9,30 @@
         </el-col>
       </el-row>
       <el-row :gutter="20" class="dataList-group"> </el-row>
+    </div> -->
+  
+    <!-- vue集成easyplayer https://www.npmjs.com/package/@easydarwin/easyplayer -->
+    <!-- https://blog.csdn.net/weixin_45906632/article/details/128560735 -->
+    <div v-for="(item,index) in url_List" :key="index" style="color: black; width: 70%; height: 400px;">
+      {{ item.name }}
+      <easy-player 
+        ref="videoplay" 
+        live
+        muted
+        autoplay
+        :video-url="item.url"
+        style="width: 100%; height: 100%"
+      >
+      </easy-player>
     </div>
+    
   </div>
 
 </template>
 
 <script setup lang="ts">
 import { ref, onMounted } from 'vue'
-import { inboundDataList, outboundDataList } from './tempData'
+import { inboundDataList, outboundDataList, url_List, url_List_1 } from './tempData'
 import scrollTable from "./scrollTable.vue";
 
 
